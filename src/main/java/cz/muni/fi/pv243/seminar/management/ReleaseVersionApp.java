VaclavChalupa/a4m36jee-2014-management-seminar

[management-seminar] Initial version of management & monitoring seminar

src/main/java/cz/ctu/fee/a4m36jee/seminar/management/ReleaseVersionApp.java
@@ -1,0 +1,51 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.management;
+
+import java.net.InetAddress;
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.dmr.ModelNode;
+
+/**
+ * WildFly8 Java API example - get release version
+ *
+ * @author Rostislav Svoboda
+ * @author Jitka Kozana
+ */
+public class ReleaseVersionApp {
+
+    public static void main(String[] args) throws Exception {
+
+        ModelControllerClient client = ModelControllerClient.Factory.create(InetAddress.getByName("localhost"), 9990);
+
+        ModelNode op = new ModelNode();
+
+        // set operation
+
+        ModelNode returnVal = client.execute(op);
+
+
+        // display results
+
+        client.close();
+    }
+}

src/main/java/cz/ctu/fee/a4m36jee/seminar/management/RemoteConnectionApp.java
@@ -1,0 +1,42 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.management;
+
+/**
+ * WildFly8 Java API example - connect to remote WildFly8 instance
+ *
+ * @author Rostislav Svoboda
+ * @author Jitka Kozana
+ */
+public class RemoteConnectionApp {
+
+    public static void main(String[] args) throws Exception {
+
+        // connect to remote WildFly8 instance
+
+        // execute any command
+
+        // print results
+
+        // close client
+    }
+}

src/main/java/cz/ctu/fee/a4m36jee/seminar/management/ResourcesDescriptionRemotingSubsystemApp.java
@@ -1,0 +1,50 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.management;
+
+import java.net.InetAddress;
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.dmr.ModelNode;
+
+/**
+ * WildFly8 Java API example - get resources description for Remoting subsystem
+ *
+ * @author Rostislav Svoboda
+ * @author Jitka Kozana
+ */
+public class ResourcesDescriptionRemotingSubsystemApp {
+
+    public static void main(String[] args) throws Exception {
+
+        ModelControllerClient client = ModelControllerClient.Factory.create(InetAddress.getByName("localhost"), 9990);
+
+        ModelNode op = new ModelNode();
+
+        // set command
+
+        ModelNode returnVal = client.execute(op);
+
+        // display results
+
+        client.close();
+    }
+}

src/main/java/cz/ctu/fee/a4m36jee/seminar/management/ResourcesRecursivelyApp.java
@@ -1,0 +1,50 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.management;
+
+import java.net.InetAddress;
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.dmr.ModelNode;
+
+/**
+ * WildFly8 Java API example - get resources recursively including runtime data
+ *
+ * @author Rostislav Svoboda
+ * @author Jitka Kozana
+ */
+public class ResourcesRecursivelyApp {
+
+    public static void main(String[] args) throws Exception {
+
+        ModelControllerClient client = ModelControllerClient.Factory.create(InetAddress.getByName("localhost"), 9990);
+
+        ModelNode op = new ModelNode();
+
+        // set command
+
+        ModelNode returnVal = client.execute(op);
+
+        // display results
+
+        client.close();
+    }
+}

qa/pv243-2015-management-seminar

src/main/java/cz/muni/fi/pv243/seminar/management/ReleaseVersionApp.java
@@ -1,0 +1,52 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.management;
+
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.dmr.ModelNode;
+
+import java.net.InetAddress;
+
+/**
+ * WildFly8 Java API example - get release version
+ *
+ * @author Rostislav Svoboda
+ * @author Jitka Kozana
+ */
+public class ReleaseVersionApp {
+
+    public static void main(String[] args) throws Exception {
+
+        ModelControllerClient client = ModelControllerClient.Factory.create(InetAddress.getByName("localhost"), 9990);
+
+        ModelNode op = new ModelNode();
+
+        // set operation
+
+        ModelNode returnVal = client.execute(op);
+
+
+        // display results
+
+        client.close();
+    }
+}

src/main/java/cz/muni/fi/pv243/seminar/management/RemoteConnectionApp.java
@@ -1,0 +1,42 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.management;
+
+/**
+ * WildFly8 Java API example - connect to remote WildFly8 instance
+ *
+ * @author Rostislav Svoboda
+ * @author Jitka Kozana
+ */
+public class RemoteConnectionApp {
+
+    public static void main(String[] args) throws Exception {
+
+        // connect to remote WildFly8 instance
+
+        // execute any command
+
+        // print results
+
+        // close client
+    }
+}

src/main/java/cz/muni/fi/pv243/seminar/management/ResourcesDescriptionRemotingSubsystemApp.java
@@ -1,0 +1,51 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.management;
+
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.dmr.ModelNode;
+
+import java.net.InetAddress;
+
+/**
+ * WildFly8 Java API example - get resources description for Remoting subsystem
+ *
+ * @author Rostislav Svoboda
+ * @author Jitka Kozana
+ */
+public class ResourcesDescriptionRemotingSubsystemApp {
+
+    public static void main(String[] args) throws Exception {
+
+        ModelControllerClient client = ModelControllerClient.Factory.create(InetAddress.getByName("localhost"), 9990);
+
+        ModelNode op = new ModelNode();
+
+        // set command
+
+        ModelNode returnVal = client.execute(op);
+
+        // display results
+
+        client.close();
+    }
+}

src/main/java/cz/muni/fi/pv243/seminar/management/ResourcesRecursivelyApp.java
@@ -1,0 +1,51 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.management;
+
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.dmr.ModelNode;
+
+import java.net.InetAddress;
+
+/**
+ * WildFly8 Java API example - get resources recursively including runtime data
+ *
+ * @author Rostislav Svoboda
+ * @author Jitka Kozana
+ */
+public class ResourcesRecursivelyApp {
+
+    public static void main(String[] args) throws Exception {
+
+        ModelControllerClient client = ModelControllerClient.Factory.create(InetAddress.getByName("localhost"), 9990);
+
+        ModelNode op = new ModelNode();
+
+        // set command
+
+        ModelNode returnVal = client.execute(op);
+
+        // display results
+
+        client.close();
+    }
+}